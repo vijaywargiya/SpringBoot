4abc2d423428a3d3c6d3acc3dd9c0e9c6a2fd957
Shivam Vijaywargiya <shivamvijaywargiya@gmail.com>
2020-07-24 11:57:15 +0530

    CRUD APIs

--- initial/src/main/java/com/example/restservice/GreetingController.java
+++ initial/src/main/java/com/example/restservice/GreetingController.java
@@ -1,19 +1,49 @@
 package com.example.restservice;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    Map<Long, Greeting> greetings = new HashMap<Long, Greeting>();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Map<Long, Greeting> list(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return greetings;
     }
+
+    @PostMapping("/greeting")
+    public Greeting create(@RequestParam(value = "name", defaultValue = "World") String name) {
+        Greeting greeting =  new Greeting(counter.incrementAndGet(), String.format(template, name));
+        greetings.put(greeting.getId(), greeting);
+        return greeting;
+    }
+
+
+    @GetMapping("/greeting/{id}")
+    public Greeting get(@PathVariable Long id) {
+        return greetings.get(id);
+    }
+
+    @PutMapping("/greeting/{id}")
+    public Greeting get(@RequestParam(value = "name", defaultValue = "World") String name, @PathVariable Long id) {
+        Greeting greeting =  new Greeting(id, String.format(template, name));
+        greetings.put(id, greeting);
+        return greeting;
+    }
+
+    @DeleteMapping("/greeting/{id}")
+    public boolean delete(@PathVariable Long id) {
+        greetings.remove(id);
+        return true;
+    }
+
+
 }
